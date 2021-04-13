9dda527bb787a4cb4f8448ea39b7bc843e965750
Rezart Qelibari <gast-kontakt@astzweig.de>
2021-04-13 22:27:40 +0200

    Add test_distanceOfSameWordIsZero

--- 02-Levenshtein/test_levenshtein.py
+++ 02-Levenshtein/test_levenshtein.py
@@ -4,3 +4,7 @@ from levenshtein import edit_distance
 class TestLevenshteinDistance(unittest.TestCase):
     def test_distanceOfTwoEmptyWordsIsZero(self):
         self.assertEqual(0, edit_distance('', ''))
+
+    def test_distanceOfSameWordIsZero(self):
+        word = 'whatwg'
+        self.assertEqual(0, edit_distance(word, word))
