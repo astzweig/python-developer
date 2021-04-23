dbc24734874da7ec2bff747aa605640929c7cb29
Rezart Qelibari <gast-kontakt@astzweig.de>
2021-04-23 20:53:24 +0200

    Refactor tests

--- 03-Prime-Factorization/test_prime_factorization.py
+++ 03-Prime-Factorization/test_prime_factorization.py
@@ -5,14 +5,12 @@ class TestGetPrimeFactors(unittest.TestCase):
     def test_returnsAnEmptyListWhenNumberIsOne(self):
         self.assertEqual([], get_prime_factors(1))
 
-    def test_returnsListWithNumberTwoWhenNumberIsTwo(self):
-        self.assertEqual([2], get_prime_factors(2))
-
-    def test_returnsListWithNumberThreeWhenNumberIsThree(self):
-        self.assertEqual([3], get_prime_factors(3))
+    def test_returnsListWithNumberWhenNumberIsPrime(self):
+        for prime in [2, 3, 5, 7]:
+            self.assertEqual([prime], get_prime_factors(prime))
 
     def test_returnsListWithTwoNumberTwosWhenNumberIsFour(self):
         self.assertEqual([2, 2], get_prime_factors(4))
 
-    def test_returnsListWithNumberFiveWhenNumberIsFive(self):
-        self.assertEqual([5], get_prime_factors(5))
+    def test_returnsListWithNumberTwoAndThreeWhenNumberIsSix(self):
+        self.assertEqual([2, 3], get_prime_factors(6))
