8837ed95fde1fbe5d3339ea54d0f8de85c8def43
Rezart Qelibari <gast-kontakt@astzweig.de>
2021-04-23 22:23:17 +0200

    Test returnsSumIfOnlyOneRoundIsGiven

--- 04-Tenis-Points/tenis_points.py
+++ 04-Tenis-Points/tenis_points.py
@@ -1,2 +1,9 @@
-def get_tenis_points_of_all_players(points_of_player: list[int]) -> (int, int):
-    return (0, 0)
+def get_tenis_points_of_all_players(points_of_player_one: list[int]) -> (int, int):
+    points_of_player_two = calculate_opposite_points(points_of_player_one)
+    return sum(points_of_player_one), sum(points_of_player_two)
+
+def calculate_opposite_points(points: list[int]) -> list[int]:
+    opposite_points = []
+    for point in points:
+        opposite_points.append(3 - point)
+    return opposite_points
--- 04-Tenis-Points/test_tenis_points.py
+++ 04-Tenis-Points/test_tenis_points.py
@@ -4,3 +4,7 @@ from tenis_points import get_tenis_points_of_all_players
 class TestGetTenisPointsForAllPlayers(unittest.TestCase):
     def test_returnsZeroIfGivenEmptyList(self):
         self.assertEqual((0, 0), get_tenis_points_of_all_players([]))
+
+    def test_returnsSumIfOnlyOneRoundIsGiven(self):
+        result = get_tenis_points_of_all_players([1])
+        self.assertEqual((1, 2), result)
