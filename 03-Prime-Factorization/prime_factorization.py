dabdc3f08a1274a3a1f09f53fef988ce1fccfe65
Rezart Qelibari <gast-kontakt@astzweig.de>
2021-04-23 20:40:09 +0200

    Test returnsListWithNumberTwoWhenNumberIsTwo

--- 03-Prime-Factorization/prime_factorization.py
+++ 03-Prime-Factorization/prime_factorization.py
@@ -1,2 +1,4 @@
 def get_prime_factors(number: int) -> list[int]:
+    if (number == 2):
+        return [2]
     return []
--- 03-Prime-Factorization/test_prime_factorization.py
+++ 03-Prime-Factorization/test_prime_factorization.py
@@ -4,3 +4,6 @@ from prime_factorization import get_prime_factors
 class TestGetPrimeFactors(unittest.TestCase):
     def test_returnsAnEmptyListWhenNumberIsOne(self):
         self.assertEqual([], get_prime_factors(1))
+
+    def test_returnsAListWithTheNumberTwoWhenNumberIsTwo(self):
+        self.assertEqual([2], get_prime_factors(2))
