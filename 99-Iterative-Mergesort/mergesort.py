6d808a1456c74d7b29c6048332989874fb796dce
Rezart Qelibari <gast-kontakt@astzweig.de>
2021-04-25 23:32:02 +0200

    Test returnsEmptyListWhenGivenEmptyList

--- 99-Iterative-Mergesort/mergesort.py
+++ 99-Iterative-Mergesort/mergesort.py
@@ -1,6 +1,17 @@
+from math import ceil
+
 def iterative_mergesort(lst: list[int]) -> list[int]:
-    if len(lst) > 1:
-        a, b = tuple(lst)
-        if (a > b):
-            return [b, a]
+    for index in range(0, len(lst), 1):
+        pass
+
+    for index in range(0, len(lst), 2):
+        length_list = 2
+        merge_list(lst[index:index + length_list])
+
+    for index in range(0, len(lst), 4):
+        length_list = 4
+        merge_list(lst[index:index + length_list])
+    return lst
+
+def merge_list(lst: list[int], left: int, right: int):
     return lst
--- 99-Iterative-Mergesort/test_mergesort.py
+++ 99-Iterative-Mergesort/test_mergesort.py
@@ -1,16 +1,30 @@
 import unittest
-from mergesort import iterative_mergesort as mergesort
+from mergesort import iterative_mergesort as mergesort, merge_list
 
 class TestIterativeMergeSort(unittest.TestCase):
+    @unittest.skip("Implement merge_list first.")
     def test_returnsEmptyListWhenGivenEmptyList(self):
         empty_list = []
         self.assertEqual(empty_list, mergesort(empty_list))
 
+    @unittest.skip("Implement merge_list first.")
     def test_returnsUnchangedListWhenGivenSingleElementList(self):
         list_with_one_element = [1]
         self.assertEqual(list_with_one_element, mergesort(list_with_one_element))
 
+    @unittest.skip("Implement merge_list first.")
     def test_sortsListWithTwoElements(self):
         list_to_sort = [4, 1]
         sorted_list = [1, 4]
         self.assertEqual(sorted_list, mergesort(list_to_sort))
+
+    @unittest.skip("Implement merge_list first.")
+    def test_sortsListWithFourElements(self):
+        list_to_sort = [4, 1, -2, 3]
+        sorted_list = [-2, 1, 3, 4]
+        self.assertEqual(sorted_list, mergesort(list_to_sort))
+
+
+class TestMergeList(unittest.TestCase):
+    def test_returnsEmptyListWhenGivenEmptyList(self):
+        self.assertEqual([], merge_list([], 0, 0))
