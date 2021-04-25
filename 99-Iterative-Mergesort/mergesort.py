2b23adb2ff1cfc1c8a51ab886cd952c78c2fdb8e
Rezart Qelibari <gast-kontakt@astzweig.de>
2021-04-25 22:17:15 +0200

    Test sortsListWithTwoElements

--- 99-Iterative-Mergesort/mergesort.py
+++ 99-Iterative-Mergesort/mergesort.py
@@ -1,2 +1,6 @@
 def iterative_mergesort(lst: list[int]) -> list[int]:
+    if len(lst) > 1:
+        a, b = tuple(lst)
+        if (a > b):
+            return [b, a]
     return lst
--- 99-Iterative-Mergesort/test_mergesort.py
+++ 99-Iterative-Mergesort/test_mergesort.py
@@ -9,3 +9,8 @@ class TestIterativeMergeSort(unittest.TestCase):
     def test_returnsUnchangedListWhenGivenSingleElementList(self):
         list_with_one_element = [1]
         self.assertEqual(list_with_one_element, mergesort(list_with_one_element))
+
+    def test_sortsListWithTwoElements(self):
+        list_to_sort = [4, 1]
+        sorted_list = [1, 4]
+        self.assertEqual(sorted_list, mergesort(list_to_sort))
